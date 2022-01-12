f3280486b9db0a4247e88ce3bcb9ac4894e992a0
Auri Martins <aurimsjr@gmail.com>
2022-01-12 02:13:41 -0300

    Adicao Projeto Conta Bancaria

---
+++ Projetos_JAVA/controleBancario/build/classes/controleBancario/ControleConta.rs
@@ -0,0 +1,1 @@
+atividadeum.ControleConta
---
+++ Projetos_JAVA/controleBancario/src/controlebancario/ContaPoupanca.java
@@ -0,0 +1,59 @@
+package controlebancario;
+public class ContaPoupanca extends Conta{
+    double valorSaque = 50;
+    
+    public ContaPoupanca(int numero, int agencia, Cliente cliente) {
+        super(numero, agencia, cliente);
+    }
+    
+    @Override
+    public void sacar(){
+        if (this.getNumero() == 0 || this.getAgencia() == 0){
+             System.out.println("**************************************");
+             System.out.println("*           CONTA INVÁLIDA           *");
+             System.out.println("**************************************");
+        }else{
+            String saldoFormatado = String.format("%.2f", saldo);//
+            String saqueFormatado = String.format("%.2f", valorSaque);
+            if (this.getSaldo() < this.valorSaque){
+                System.out.println("              !!! ATENÇÃO !!!\n     "
+                        + "$ SALDO INSUFICIENTE PARA SAQUES. $       ");
+                System.out.println("********************************************");
+            }else{
+                this.saldo -= this.valorSaque;
+                System.out.println("\n********************************************");
+                System.out.println("°          [ SAQUE  C.POUPANÇA ]           °");
+                System.out.println("Conta: "+this.getNumero()+"                         Agência: "+this.getAgencia());
+                System.out.println("DESCRIÇÃO..........................VALOR(R$)");
+                System.out.println("SAQUE................................ "+ saqueFormatado +"          ");
+                System.out.println("--------------------------------------------");
+                System.out.print("SALDO ANTERIOR........................"+saldoFormatado);//
+                System.out.print("\nSALDO ATUAL........................... ");
+                System.out.format("%.2f",getSaldo());
+                System.out.println("\n********************************************");
+            }
+        }
+    }
+    public double getValorSaque() {
+        return valorSaque;
+    }
+    public void setValorSaque(double valorSaque) {
+        this.valorSaque = valorSaque;
+    }
+}
+
+/*
+Conta poupança
+-Deverá herdar de conta;
+- Não deverá permitir sacar um valor superior ao saldo;
+*/
+
+/*
+Conta:
+- Número da conta e agência deverá ser maior que 0;
+- Deverá ter um construtor com número, agência e cliente;
+- Deverá iniciar um saldo 0;
+- Deverá ter um método de sacar e depositar;
+- O atributo saldo deverá ter apenas método get;
+- O método sacar deverá ser abstrato;
+*/

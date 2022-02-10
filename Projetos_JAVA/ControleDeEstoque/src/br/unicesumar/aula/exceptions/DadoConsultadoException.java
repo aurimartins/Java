f8e13dc7a30f35c466cc233acec58e1bb666c66d
Auri Martins <aurimsjr@gmail.com>
2022-02-10 09:30:49 -0300

    Projeto JavaFx-jdbc criado

--- Projetos_JAVA/ControleDeEstoque/src/br/unicesumar/aula/exceptions/DadoConsultadoException.java
+++
@@ -1,7 +0,0 @@
-package br.unicesumar.aula.exceptions;
- 
-public class DadoConsultadoException extends Exception{
-    public DadoConsultadoException(String message) {
-        super(message);
-    }
-}
